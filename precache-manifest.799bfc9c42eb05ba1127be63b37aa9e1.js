41cadd8fb67338c5016b842f4ac7699a3e02fdb3
shangweihung <alannn1230@gmail.com>
2020-03-26 16:27:04 -0700

    Updates

---
+++ precache-manifest.799bfc9c42eb05ba1127be63b37aa9e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41ae9e1eaedc54ab6dd9c5e90589494",
+    "url": "/react-build-youtube/index.html"
+  },
+  {
+    "revision": "f640b3be2d133d3b221f",
+    "url": "/react-build-youtube/static/js/2.e5baee14.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/react-build-youtube/static/js/2.e5baee14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd008e9de6f47b9af20",
+    "url": "/react-build-youtube/static/js/main.4dace3bd.chunk.js"
+  },
+  {
+    "revision": "07e8f521037b721d7fc5",
+    "url": "/react-build-youtube/static/js/runtime-main.3926f2a8.js"
+  }
+]);
